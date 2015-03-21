24ef5b132fb2e7cfd31cc582c4c2a7b6fcf751e5
richelbilderbeek <richel@richelbilderbeek.nl>
2015-03-22 00:03:48 +0100

    Using the Time library, version 1.1

--- ApproxyClock/ApproxyClock.ino
+++ ApproxyClock/ApproxyClock.ino
@@ -4,10 +4,11 @@
   (C) 2015 Richel Bilderbeek
 
 2015-03-21: v.1.0: Initial version
+2015-03-22: v.1.1: use of Time library
 
 */
 
-//#include <Time.h>
+#include <Time.h>
 
 const int red_pin = 5;
 const int green_pin = 6;
@@ -28,80 +29,19 @@ void OnError(const String& error_message)
   }
 }
 
-class Time
-{
-  public:
-  Time(const int delta_secs, const int delta_mins, const int delta_hours, const long my_millis = -1)
-    : m_total_msecs(my_millis == -1 ? millis() : my_millis)
-  {
-    #ifndef NDEBUG
-    if (m_total_msecs < 0) { OnError("Time: m_total_msecs < 0, m_total_msecs = " + String(m_total_msecs)); }
-    if (delta_secs <  0) { OnError("Time: delta_secs <  0, delta_secs = " + String(delta_secs)); }
-    if (delta_secs > 59) { OnError("Time: delta_secs > 59, delta_secs = " + String(delta_secs)); }
-    if (delta_mins <  0) { OnError("Time: delta_mins <  0, delta_mins = " + String(delta_mins)); }
-    if (delta_mins > 59) { OnError("Time: delta_mins > 59, delta_mins = " + String(delta_mins)); }
-    if (delta_hours <  0) { OnError("Time: delta_hours <  0, delta_hours = " + String(delta_hours)); }
-    if (delta_hours > 23) { OnError("Time: delta_hours > 23, delta_hours = " + String(delta_hours)); }
-    #endif // NDEBUG
-
-    m_total_secs = delta_secs + (m_total_msecs / 1000);
-    m_secs = m_total_secs % 60;
-    m_total_mins = delta_mins + (m_total_secs / 60);
-    m_mins = m_total_mins % 60;
-    m_total_hours = delta_hours + (m_total_mins / 60);
-    m_hours = m_total_hours % 24;    
-
-    #ifndef NDEBUG
-    if (m_total_secs  < 0) { OnError("Time: m_total_secs < 0, m_total_secs = "   + String(m_total_secs )); }
-    if (m_total_mins  < 0) { OnError("Time: m_total_mins < 0, m_total_mins = "   + String(m_total_mins )); }
-    if (m_total_hours < 0) { OnError("Time: m_total_hours < 0, m_total_hours = " + String(m_total_hours)); }
-
-    if (m_secs > 59) { OnError("Time: m_secs > 59, delta_secs = " + String(delta_secs)); }
-    if (m_secs <  0) { OnError("Time: m_secs <  0, delta_secs = " + String(delta_secs)); }
-    if (m_secs > 59) { OnError("Time: m_secs > 59, delta_secs = " + String(delta_secs)); }
-    if (m_mins <  0) { OnError("Time: m_mins <  0, delta_mins = " + String(delta_mins)); }
-    if (m_mins > 59) { OnError("Time: m_mins > 59, delta_mins = " + String(delta_mins)); }
-    if (m_hours <  0) { OnError("Time: m_hours <  0, delta_hours = " + String(delta_hours)); }
-    if (m_hours > 23) { OnError("Time: m_hours > 23, delta_hours = " + String(delta_hours)); }
-    #endif // NDEBUG
-  }
-  int GetHours() const { return m_hours; }
-  int GetMins() const { return m_mins; }
-  int GetSecs() const { return m_secs; }
-  private:
-  int m_hours;
-  int m_mins;
-  int m_secs; 
-  const long m_total_msecs; //Must be long
-  long m_total_secs; //Must be long, a day has more than 65536 secs
-  int m_total_mins;
-  int m_total_hours;
-};
-
-Time GetTime();
-
-int delta_hours = 0;
-int delta_mins = 0;
-int delta_secs = 0;
-
-Time GetTime()
-{
-  return Time(delta_secs,delta_mins,delta_hours);
-}
-
 void TestTime()
 {
-  Time(0,0,0);
-  Time(0,0,0,0);
-  if (Time(0,0,0,1000).GetSecs() != 1) { OnError("TestTime #1"); }
-  if (Time(0,0,0, 60000).GetMins() != 1) { OnError("TestTime #2"); }
-  if (Time(0,0,0,3600000).GetHours() != 1) { OnError("TestTime #3"); }
-  if (Time(59,0,0,1000).GetSecs() != 0) { OnError("TestTime #4"); }
-  if (Time(59,0,0,1000).GetMins() != 1) { OnError("TestTime #5"); }
-  if (Time(0,59,0,60000).GetMins() != 0) { OnError("TestTime #6"); }
-  if (Time(0,59,0,60000).GetHours() != 1) { OnError("TestTime #7"); }
-  if (Time(0,0,22,3600000).GetHours() != 23) { OnError("TestTime #8"); }
-  if (Time(0,0,23,3600000).GetHours() !=  0) { OnError("TestTime #9"); }
+  //  setTime(hours, minutes, seconds, days, months, years);
+  const int hours = 11;
+  const int minutes = 22;
+  const int seconds = 33;
+  const int days = 0;
+  const int months = 0;
+  const int years = 0;
+  setTime(hours, minutes, seconds, days, months, years);
+  if (hour() != hours) { OnError("TestTime #1"); }
+  if (minute() != minutes) { OnError("TestTime #1"); }
+  if (second() != seconds) { OnError("TestTime #1"); }
 }
 
 void setup()
@@ -115,9 +55,9 @@ void setup()
   pinMode(error_pin,OUTPUT);
   Serial.begin(9600); //Cannot be used: chip is used stand-alone
   #ifndef NDEBUG
-  Serial.println("ApproxyClock v. 1.0 (debug version)");
+  Serial.println("ApproxyClock v. 1.1 (debug version)");
   #else //NDEBUG
-  Serial.println("ApproxyClock v. 1.0 (release version)");
+  Serial.println("ApproxyClock v. 1.1 (release version)");
   #endif //NDEBUG
   TestTime();
 }
@@ -176,47 +116,7 @@ void SetTimeFromSerial()
     Serial.println("Seconds must be in range [0,59]");
     return;
   }
-  const int pre_delta_hour = GetTime().GetHours() - delta_hours;
-  delta_hours = ((h - pre_delta_hour) + 24) % 24;
-
-  const int pre_delta_mins = GetTime().GetMins() - delta_mins;
-  delta_mins = ((m - pre_delta_mins) + 60) % 60;
-
-  const int pre_delta_secs = GetTime().GetSecs() - delta_secs;
-  delta_secs = ((s - pre_delta_secs) + 60) % 60;
-  /*
-  #ifndef NDEBUG
-  const String time_indended = String(h) + String(":") + String(m) + String(":") + String(s);
-  const String time_realized 
-    = String(GetTime().GetHours()) 
-    + String(":") 
-    + String(GetTime().GetMins()) 
-    + String(":") 
-    + String(GetTime().GetSecs()
-  );
-  const String debug_msg 
-    = String("time_indended (") 
-    + time_indended 
-    + String("), time_realized (") 
-    + time_realized 
-    + String(") ")
-  ;
-  delay(100);
-  Serial.println(debug_msg);
-  delay(100);
-  if (time_indended != time_realized)
-  {
-    const String error_msg 
-      = String("time_indended (") 
-      + time_indended 
-      + String(")!= time_realized (") 
-      + time_realized 
-      + String(")")
-    ;
-    OnError(error_msg);
-  }
-  #endif
-  */
+  setTime(h,m,s,0,0,0); 
 }
 
 void loop() 
@@ -230,11 +130,9 @@ void loop()
       SetTimeFromSerial();  
     }
     //Show the time
-    const Time t = GetTime();
-    const int s = t.GetSecs();
-    const int m = t.GetMins();
-    const int h = t.GetHours();
-
+    const int s = second();
+    const int m = minute();
+    const int h = hour();
 
     if (last_sec == s) 
     {
@@ -246,12 +144,7 @@ void loop()
 
     //if (sensors_state == state_right_sensor_pressed) 
     { 
-      //Send debug message to console window
-      const Time t = GetTime();
-      const String deltas = String(delta_hours) + ":" + String(delta_mins) + ":" + String(delta_secs);
       const String time_now = String(h) + ":" + String(m) + ":" + String(s);
-      Serial.print(deltas);
-      Serial.print(" -> ");
       Serial.println(time_now);
       delay(100);
     }
