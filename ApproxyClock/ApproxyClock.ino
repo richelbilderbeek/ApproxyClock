8e65ac5bedfe8dc042ad517365b90dba3b29b6ce
richelbilderbeek <richel@richelbilderbeek.nl>
2015-03-22 01:06:39 +0100

    Added rainbow

--- ApproxyClock/ApproxyClock.ino
+++ ApproxyClock/ApproxyClock.ino
@@ -5,14 +5,20 @@
 
 2015-03-21: v.1.0: Initial version
 2015-03-22: v.1.1: use of Time library
+2015-03-22: v.1.2: added parallel display of rainbow format
 
 */
 
 #include <Time.h>
 
-const int red_pin = 5;
-const int green_pin = 6;
-const int blue_pin = 3;
+const int blue_original_pin = 3;
+const int red_original_pin = 5;
+const int green_original_pin = 6;
+
+const int blue_rainbow_pin = 9;
+const int red_rainbow_pin = 10;
+const int green_rainbow_pin = 11;
+
 const int resetpin = 7; //The pin connected to RST, RST connected to 10 kOhm resistance connected to 5V
 const int error_pin = 13;
 
@@ -42,6 +48,7 @@ void TestTime()
   if (hour() != hours) { OnError("TestTime #1"); }
   if (minute() != minutes) { OnError("TestTime #1"); }
   if (second() != seconds) { OnError("TestTime #1"); }
+  setTime(0,0,0,0,0,0);
 }
 
 void setup()
@@ -49,9 +56,12 @@ void setup()
   digitalWrite(resetpin,HIGH);  //This line is first, so when the Arduino is reset it doesn't reset again untill it's told to
   pinMode(resetpin,OUTPUT);     //Very important, without this line resetting won't work
 
-  pinMode(red_pin,OUTPUT);
-  pinMode(green_pin,OUTPUT);
-  pinMode(blue_pin,OUTPUT);
+  pinMode(red_original_pin,OUTPUT);
+  pinMode(green_original_pin,OUTPUT);
+  pinMode(blue_original_pin,OUTPUT);
+  pinMode(red_rainbow_pin,OUTPUT);
+  pinMode(green_rainbow_pin,OUTPUT);
+  pinMode(blue_rainbow_pin,OUTPUT);
   pinMode(error_pin,OUTPUT);
   Serial.begin(9600); //Cannot be used: chip is used stand-alone
   #ifndef NDEBUG
@@ -140,7 +150,8 @@ void loop()
     }
 
     last_sec = s;
-    ShowTime(s,m,h);
+    ShowTimeOriginal(s,m,h);
+    ShowTimeRainbow(s,m,h);
 
     //if (sensors_state == state_right_sensor_pressed) 
     { 
@@ -158,7 +169,7 @@ void loop()
 }
 
 ///Show the time on all RGB LEDs
-void ShowTime(const int secs, const int mins, const int hours)
+void ShowTimeOriginal(const int secs, const int mins, const int hours)
 {
   #ifndef NDEBUG
   if (hours <  0) { OnError("ShowTime: hours <  0, hours = " + String(hours)); }
@@ -172,7 +183,54 @@ void ShowTime(const int secs, const int mins, const int hours)
   const int red_value   = map(secs,0,60,0,max_brightness);
   const int green_value = map(mins,0,60,0,max_brightness);
   const int blue_value  = map(hours,0,24,0,max_brightness);
-  analogWrite(red_pin,red_value);
-  analogWrite(green_pin,green_value);
-  analogWrite(blue_pin,blue_value);
+  analogWrite(red_original_pin,red_value);
+  analogWrite(green_original_pin,green_value);
+  analogWrite(blue_original_pin,blue_value);
+}
+
+///Show the time on all RGB LEDs using rainbow format
+// B     R     G     B 
+// +     +     +     +
+// |\   / \   / \   / 
+// | \ /   \ /   \ /
+// |  X     X     X
+// | / \   / \   / \
+// |/   \ /   \ /   \
+// +-----+-----+-----+
+// 0     0     1     2
+// 0     8     6     4
+void ShowTimeRainbow(const int secs, const int mins, const int hours)
+{
+  #ifndef NDEBUG
+  if (hours <  0) { OnError("ShowTime: hours <  0, hours = " + String(hours)); }
+  if (hours > 23) { OnError("ShowTime: hours > 23, hours = " + String(hours)); }
+  if (mins <  0) { OnError("ShowTime: mins <  0, mins = " + String(mins)); }
+  if (mins > 59) { OnError("ShowTime: mins > 59, mins = " + String(mins)); }
+  if (secs <  0) { OnError("ShowTime: secs <  0, secs = " + String(secs)); }
+  if (secs > 59) { OnError("ShowTime: secs > 59, secs = " + String(secs)); }
+  #endif // NDEBUG
+  int red_value = 0;
+  int green_value = 0;
+  int blue_value = 0;
+  if (hours < 8)
+  {
+    const double f = static_cast<double>((hours*60) + mins) / (8 * 60);
+    red_value = static_cast<int>(f * 255.0);
+    blue_value = 255 - red_value;
+  }
+  else if (hours < 16)
+  {
+    const double f = static_cast<double>(((hours-8)*60) + mins) / (8 * 60);
+    green_value = static_cast<int>(f * 255.0);
+    red_value = 255 - red_value;    
+  }
+  else if (hours < 24)
+  {
+    const double f = static_cast<double>(((hours-16)*60) + mins) / (8 * 60);
+    blue_value = static_cast<int>(f * 255.0);
+    green_value = 255 - red_value;    
+  }
+  analogWrite(red_rainbow_pin,red_value);
+  analogWrite(green_rainbow_pin,green_value);
+  analogWrite(blue_rainbow_pin,blue_value);
 }
